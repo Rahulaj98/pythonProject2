eedd72186a0ebbedf1817da08a4545679d0fbd3f
Rahul A <rahulajvit@gmail.com>
2022-05-11 17:18:04 +0530

    minor change

---
+++ sample.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import pymysql
+
+def hello_funct(z):
+    print(z)
+    print(z)
+
+
+# Press the green button in the gutter to run the script.
+hello_funct(5)
+
